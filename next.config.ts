2234ff47991d1f36b9c4451c8d9d737447d4038f
dokudami <ysdzm75@gmail.com>
2025-07-01 10:45:58 +0900

    fix: configを追加

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
+const prefix = '/survey-js';
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: 'export',
+  basePath: prefix,
+  assetPrefix: prefix,
 };
 
 export default nextConfig;
